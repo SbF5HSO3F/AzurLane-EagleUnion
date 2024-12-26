b5dfd4fc28cc2f64e7f7a183371cfa2c3518f51c
HSbF5HSO3F <jinghaoyu1030@qq.com>
2024-12-26 08:29:29 +0800

    feat(ui): 添加额外面板和顶部面板的UI支持与文本替换信息

--- Scripts/EagleUnion_Core.lua
+++ Scripts/EagleUnion_Core.lua
@@ -41,11 +41,41 @@ function EagleCore.HasBoost(techOrCivic)
     return false
 end
 
+--判断单元格是否可以放置指定单位 (GamePlay, UI)
+function EagleCore.CanHaveUnit(plot, unitdef)
+    if plot == nil then return false end
+    local canHave = true
+    for _, unit in ipairs(Units.GetUnitsInPlot(plot)) do
+        if unit then
+            local unitInfo = GameInfo.Units[unit:GetType()]
+            if unitInfo then
+                if unitInfo.IgnoreMoves == false then
+                    if unitInfo.Domain == unitdef.Domain and unitInfo.FormationClass == unitdef.FormationClass then
+                        canHave = false
+                    end
+                end
+            end
+        end
+    end
+    return canHave
+end
+
 --||=====================GamePlay=======================||--
 
 --随机数生成器，范围为[1,num+1] (GamePlay)
 function EagleCore.tableRandom(num)
     return Game.GetRandNum and (Game.GetRandNum(num) + 1) or 1
+end
+
+--对单位造成伤害，超出生命值则死亡 (GamePlay)
+function EagleCore.DamageUnit(unit, damage)
+    local maxDamage = unit:GetMaxDamage()
+    if (unit:GetDamage() + damage) >= maxDamage then
+        unit:SetDamage(maxDamage)
+        UnitManager.Kill(unit, false)
+    else
+        unit:ChangeDamage(damage)
+    end
 end
 
 --||=========================UI=========================||--
