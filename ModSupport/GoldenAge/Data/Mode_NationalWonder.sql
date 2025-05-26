c2d61e6f3c5993e8ca363c595ed0ec019e0c4ee1
HSbF5HSO3F <jinghaoyu1030@qq.com>
2025-05-26 12:21:04 +0800

    白鹰联邦：完成国家奇观适配

--- ModSupport/GoldenAge/Data/Mode_NationalWonder.sql
+++ ModSupport/GoldenAge/Data/Mode_NationalWonder.sql
@@ -2,3 +2,12 @@
 -- Author: HSbF6HSO3F
 -- DateCreated: 2025/5/26 11:05:03
 --------------------------------------------------------------
+INSERT INTO District_Adjacencies
+    (DistrictType,      			YieldChangeId)
+VALUES
+	--Santa Clara Valley
+	('DISTRICT_SANTA_CLARA_VALLEY',	'GOLDEN_NATIONAL_INDUSTRIAL_ZONE_PRODUCTION'),
+    ('DISTRICT_SANTA_CLARA_VALLEY',	'GOLDEN_NATIONAL_INDUSTRIAL_ZONE_PRODUCTION_PLUS'),
+	--Lvy League
+	('DISTRICT_IVY_LEAGUE',			'GOLDEN_NATIONAL_CAMPUS_SCIENCE'),
+    ('DISTRICT_IVY_LEAGUE',			'GOLDEN_NATIONAL_CAMPUS_SCIENCE_PLUS');
