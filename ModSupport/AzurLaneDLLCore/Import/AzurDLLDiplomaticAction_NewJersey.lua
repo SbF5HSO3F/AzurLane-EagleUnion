16cd06867e40ef0896aed6428235bf88633a1985
SbF5HSO3F <190558883+SbF5HSO3F@users.noreply.github.com>
2025-08-12 18:03:20 +0800

    新泽西：采用新方法修改外交可行性

--- ModSupport/AzurLaneDLLCore/Import/AzurDLLDiplomaticAction_NewJersey.lua
+++ ModSupport/AzurLaneDLLCore/Import/AzurDLLDiplomaticAction_NewJersey.lua
@@ -1,6 +1,6 @@
--- EagleUnionNewJerseyDLL
+-- AzurDLLDiplomaticAction_NewJersey
 -- Author: HSbF6HSO3F
--- DateCreated: 2025/8/11 16:52:59
+-- DateCreated: 2025/8/12 17:29:44
 --------------------------------------------------------------
 --||=======================include========================||--
 include('EagleCore')
@@ -18,41 +18,22 @@ local DefenseMinCB = GameInfo.DiplomaticActions['DIPLOACTION_DECLARE_PROTECTORAT
 --||=================GameEvents functions=================||--
 
 -- New Jersey Can Declare War on Liberation, Reconquest and Defense of the City-State
-function NewJerseyFreeCanDeclareWar(initiatorPlayerType, targetPlayerType, warType, oldresult)
-    if EagleCore.CheckLeaderMatched(initiatorPlayerType, 'LEADER_NEW_JERSEY_BB62') and
-        (warType == Liberation or warType == Reconquest or warType == DefenseMin) then
-        return true
-    else
-        return oldresult
-    end
-end
+-- function NewJerseyFreeCanDeclareWar(initiatorPlayerType, targetPlayerType, warType, oldresult)
+--     if EagleCore.CheckLeaderMatched(initiatorPlayerType, 'LEADER_NEW_JERSEY_BB62') and
+--         (warType == Liberation or warType == Reconquest or warType == DefenseMin) then
+--         return true
+--     else
+--         return oldresult
+--     end
+-- end
 
 -- New Jersey Free Liberation, Reconquest and Defense of the City-State Casus Belli
 function NewJerseyFreeWarCasusBelli(initiatorPlayerType, targetPlayerType, diplomaticActionType, oldresult)
     -- 是新泽西，且外交行动为宣布解放战争、收复战争或保卫城邦战争
     if EagleCore.CheckLeaderMatched(initiatorPlayerType, 'LEADER_NEW_JERSEY_BB62') and
         (diplomaticActionType == LiberationCB or diplomaticActionType == ReconquestCB or diplomaticActionType == DefenseMinCB) then
-        print('New Jersey ' .. diplomaticActionType .. ' is Vaild')
         return true
-    else
-        return oldresult
     end
 end
 
---||======================initialize======================||--
-
---Initialize
-function Initialize()
-    -----------------GameEvents-----------------
-    GameEvents.ALCanDeclareWarOn.Add(NewJerseyFreeCanDeclareWar)
-    GameEvents.ALIsDiplomaticActionValid.Add(NewJerseyFreeWarCasusBelli)
-    print('LiberationCB: ' .. LiberationCB)
-    print('ReconquestCB: ' .. ReconquestCB)
-    print('DefenseMinCB: ' .. DefenseMinCB)
-    --------------------------------------------
-    print('Initial success!')
-end
-
-include('EagleUnionNewJerseyDLL_', true)
-
-Initialize()
+table.insert(AzurDLLDiplomacy.Traits, NewJerseyFreeWarCasusBelli)
